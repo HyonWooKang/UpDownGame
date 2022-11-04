80adcec3c1da43d77439fe20ea23ff11f704b5a6
Hyon Woo Kang <hwkang@engistech.com>
2022-11-04 14:55:53 +0900

    Add hit and lose logic Add alert function

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -24,13 +24,49 @@ class ViewController: UIViewController {
         slider.setThumbImage(UIImage(imageLiteralResourceName: "slider_thumb.png"), for: .normal)
         reset()
     }
-
+    // slider 값 변경 시 처리
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
     }
-
+    
+    // 게임 결과를 알려주는 alert
+    func showAlert(message: String) {
+        
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) {
+            (action) in self.reset()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // v버튼을 눌러 랜덤 게임 진행
     @IBAction func touchUpHitButton(_ sender: UIButton) {
+
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount += 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        // 랜덤 게임
+        if randomValue == hitValue {
+            //print("You Hit!!")
+            showAlert(message: "You hit!")
+            reset()
+        } else if tryCount >= 5 {
+            //print("You lose")
+            showAlert(message: "You lose")
+            reset()
+        } else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minimumValueLable.text = String(hitValue)
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = String(hitValue)
+        }
     }
 
     @IBAction func touchUpResetButton(_ sender: UIButton) {
